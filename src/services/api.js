76799fd2d99d530bf2df069da317f34f19af5ede
Carlos Alberto <carlosjunior.alberto4@gmail.com>
2022-07-07 10:57:03 -0300

    mudando URL para requisicoes no backend

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 const axios = require('axios')
 
 const api = axios.create({
-    baseURL: 'http://localhost:9000',
+    baseURL: 'https://sleepy-basin-97464.herokuapp.com',
 });
 
 module.exports = api
